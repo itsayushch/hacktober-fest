0f47da87fb63fdee685ed555a85268511af66074
Sagar <91688324+sagar9495@users.noreply.github.com>
2021-10-01 22:55:38 +0530

    Create Squares.Java

---
+++ java/Squares.Java
@@ -0,0 +1,13 @@
+//squares of first 5 integers
+class squares
+{
+   public static void main(String args [])
+   {
+     int i,a;
+    for(i=0;i<6;i++)
+        {
+           a=i*i;
+           System.out.println( "the square of " +i +"is" +a);
+        }
+   }
+}
